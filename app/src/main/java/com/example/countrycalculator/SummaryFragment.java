d1aca7ec688d917d5131f508cdb9ee82e9c35669
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-22 19:04:09 +0530

    Create SummaryFragment.java

---
+++ app/src/main/java/com/example/countrycalculator/SummaryFragment.java
@@ -0,0 +1,15 @@
+package com.example.countrycalculator;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SummaryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_summary, container, false);
+    }
+}
