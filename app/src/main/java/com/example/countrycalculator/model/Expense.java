d1b017607d70842d802c6e523403f1bd72cc706a
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-22 19:12:34 +0530

    Create Expense.java

---
+++ app/src/main/java/com/example/countrycalculator/model/Expense.java
@@ -0,0 +1,19 @@
+package com.example.countrycalculator.model;
+
+public class Expense {
+    private String category;
+    private double amount;
+
+    public Expense(String category, double amount) {
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
