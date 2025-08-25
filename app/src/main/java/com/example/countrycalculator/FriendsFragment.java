dbd3932fac96075170946482cc3785a05d95f7fa
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-25 14:20:04 +0530

    Update FriendsFragment.java

--- app/src/main/java/com/example/countrycalculator/FriendsFragment.java
+++ app/src/main/java/com/example/countrycalculator/FriendsFragment.java
@@ -32,7 +32,7 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_friends, container, false);
 
-        editTextFriendName = view.findViewById(R.id.editTextFriendName);
+        editTextFriendName = view.findViewById(R.id.et_friend_name);
         buttonAddFriend = view.findViewById(R.id.buttonAddFriend);
         recyclerViewFriends = view.findViewById(R.id.recyclerViewFriends);
 
