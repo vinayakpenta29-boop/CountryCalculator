3fc948408f8a5a6a6261dcb5421617aa302cc2aa
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-25 12:05:55 +0530

    Update ExpensesFragment.java

--- app/src/main/java/com/example/countrycalculator/ExpensesFragment.java
+++ app/src/main/java/com/example/countrycalculator/ExpensesFragment.java
@@ -1,5 +1,4 @@
-package com.ravi.countrycalculator; // keep your package name
-
+package com.example.countrycalculator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
