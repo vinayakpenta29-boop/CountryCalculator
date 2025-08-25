765ed862252a26c33b675cae3575967466ce7a4f
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-25 23:31:59 +0530

    Create ViewPagerAdapter.java

---
+++ app/src/main/java/com/example/countrycalculator/adapter/ViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.countrycalculator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new FriendsFragment();
+            case 1:
+                return new ExpensesFragment();
+            case 2:
+                return new SummaryFragment();
+            case 3:
+                return new TransactionsFragment(); // NEW
+            default:
+                return new FriendsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4; // updated count
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Friends";
+            case 1:
+                return "Expenses";
+            case 2:
+                return "Summary";
+            case 3:
+                return "Transactions"; // NEW
+            default:
+                return "";
+        }
+    }
+}
