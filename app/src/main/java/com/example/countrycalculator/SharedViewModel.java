0d3df2f1b2e7ee1c2aba727dc586f8e6300e234a
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-27 23:25:53 +0530

    Update SharedViewModel.java

--- app/src/main/java/com/example/countrycalculator/SharedViewModel.java
+++ app/src/main/java/com/example/countrycalculator/SharedViewModel.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.countrycalculator.R;
 import com.example.countrycalculator.model.Friend;
 import com.example.countrycalculator.model.Expense;
 import com.example.countrycalculator.model.Balance;
