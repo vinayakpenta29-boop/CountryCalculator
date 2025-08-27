bc62fcece72497f23775f092a6f9528e6e125bf1
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-27 23:39:08 +0530

    Update FriendsFragment.java

--- app/src/main/java/com/example/countrycalculator/fragments/FriendsFragment.java
+++ app/src/main/java/com/example/countrycalculator/fragments/FriendsFragment.java
@@ -14,6 +14,7 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.countrycalculator.R;
 import com.example.countrycalculator.adapter.FriendsAdapter;
 import com.example.countrycalculator.model.Friend;
 import com.example.countrycalculator.viewmodel.SharedViewModel;
