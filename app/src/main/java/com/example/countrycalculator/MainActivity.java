ee1d103279a2c8f8aeb214bc1f1ce6408585bec8
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-27 19:32:01 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/countrycalculator/MainActivity.java
+++ app/src/main/java/com/example/countrycalculator/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.example.countrycalculator.fragments.ExpensesFragment;
+import com.example.countrycalculator.fragments.FriendsFragment;
+import com.example.countrycalculator.fragments.SummaryFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
