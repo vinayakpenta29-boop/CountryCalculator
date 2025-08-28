f7eba777dffa5190b8adfe2ef50ba024c1b40a5b
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-28 23:50:44 +0530

    Update FriendsAdapter.java

--- app/src/main/java/com/example/countrycalculator/adapter/FriendsAdapter.java
+++ app/src/main/java/com/example/countrycalculator/adapter/FriendsAdapter.java
@@ -8,8 +8,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-import com.example.countrycalculator.R;
 import com.example.countrycalculator.model.Friend;
 
 import java.util.List;
