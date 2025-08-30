383bdd6d1f9ae6d0fc8c7e2f26448e780554650f
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-30 22:35:44 +0530

    Update ExpenseAdapter.java

--- app/src/main/java/com/example/countrycalculator/adapter/ExpenseAdapter.java
+++ app/src/main/java/com/example/countrycalculator/adapter/ExpenseAdapter.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.countrycalculator.R;
+
 import com.example.countrycalculator.model.Expense;
 
 import java.util.ArrayList;
