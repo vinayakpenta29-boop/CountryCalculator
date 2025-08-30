7be0698b97bdba52451d497ed54d9c4a60123e04
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-08-30 18:42:26 +0530

    Update ExpensesFragment.java

--- app/src/main/java/com/example/countrycalculator/fragments/ExpensesFragment.java
+++ app/src/main/java/com/example/countrycalculator/fragments/ExpensesFragment.java
@@ -17,6 +17,8 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.countrycalculator.R;
+
 import com.example.countrycalculator.adapter.ExpenseAdapter;
 import com.example.countrycalculator.model.Expense;
 import com.example.countrycalculator.viewmodel.SharedViewModel;
